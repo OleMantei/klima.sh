a3e0682b7b5bd37922b171c744f88f177d01fbc7
OleMantei <48588093+OleMantei@users.noreply.github.com>
2023-12-19 15:57:56 +0100

    add nextui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <NextUIProvider>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme={
+          // TODO: add color scheme toggle to ui
+          window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light"
+        }
+      >
+        <App />
+      </NextThemesProvider>
+    </NextUIProvider>
+  </React.StrictMode>
+);
