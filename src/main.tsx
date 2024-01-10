c6fc64028958d0af918723f1c73fb44d06f83b6c
Patrick SCHNITTGER <patrick.schnittger@gmail.com>
2024-01-10 17:41:53 +0100

    Added test comment
    Just wanted to see if i can push

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
       <BrowserRouter>
+      {/* test */}
         <App />
       </BrowserRouter>
     </RecoilRoot>
