ce7e913eb74e96945632024d3ceb104c747abddf
Patrick SCHNITTGER <patrick.schnittger@gmail.com>
2024-01-30 13:02:28 +0100

    Added Names to Help-page

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -19,11 +19,28 @@ export const Help = () => {
         <div>Help coming soon</div>
         <br />
         <div>
-          Dies ist ein privates Studierendenprojekt der{' '}
+          Dies ist ein privates Studierendenprojekt welches von:
+          <br />
+          <br />
+          Ole Mantei,
+          <br />
+          Leon Raule,
+          <br />
+          Paul Siraf,
+          <br />
+          Simon Alers,
+          <br />
+          Patrick Schnittger
+          <br />
+          <br />
+          an der{' '}
           <a href="https://www.uni-luebeck.de" className="underline">
             Universität zu Lübeck
-          </a>
-          . Es ist zum jetzigen Zeitpunkt nicht für die Öffentlichkeit bestimmt.
+          </a>{' '}
+          im Rahmen einer Lehrveranstalltung erstellt wurde.
+          <br />
+          <br />
+          Es ist zum jetzigen Zeitpunkt nicht für die Öffentlichkeit bestimmt.
         </div>
       </div>
     </>
